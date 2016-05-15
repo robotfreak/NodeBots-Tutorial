e956163e2a6e2b2144e7994aa35dda92fc102dfa
RobotFreak <p.recktenwald@gmail.com>
2016-05-15 22:41:10 +0200

    Update ldr-sensor.js

--- code/ldr-sensor.js
+++ code/ldr-sensor.js
@@ -4,6 +4,7 @@ var board = new five.Board();
 board.on("ready", function() {
   var digits = new five.Led.Digits({
     controller: "HT16K33",
+    address: "0x74"
   });
   var sensor = new five.Sensor({
     pin: "A2",
