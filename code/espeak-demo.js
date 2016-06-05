97a228490883e914fcba92b77f5eb659d4ec74ef
Peter Recktenwald <p.recktenwald@gmail.com>
2016-06-05 13:06:47 +0200

    espeak updated to node-espeak

--- code/espeak-demo.js
+++ code/espeak-demo.js
@@ -1,18 +1,8 @@
-var espeak = require('espeak');
+var espeak = require('node-espeak');
 
-// optionally set the path to the `espeak` cli program if it's not in your PATH
-//espeak.cmd = '/usr/bin/espeak';
+espeak.initialize();
 
-espeak.speak('hello world', function(err, wav) {
-  if (err) return console.error(err);
-  
-  // get the raw binary wav data
-  var buffer = wav.buffer;
-  
-  // get a base64-encoded data URI
-  var dataUri = wav.toDataUri();
+espeak.onVoice(function(wav, samples, samplerate) {
 });
-
-// optionally add custom cli arguments for things such as pitch, speed, wordgap, etc.
-espeak.speak('hello world, slower', ['-p 60', '-s 90', '-g 30'], function(err, wav) {});
+espeak.speak('hello world');
 
